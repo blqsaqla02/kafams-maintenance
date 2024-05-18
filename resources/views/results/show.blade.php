df66d711950b0f315c0f5db2216660a62c508c86
blqsaqla02 <133661551+blqsaqla02@users.noreply.github.com>
2024-05-18 15:48:38 +0800

    show blade

---
+++ resources/views/results/show.blade.php
@@ -0,0 +1,41 @@
+@extends('results.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Students Result</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('results.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $result->student_name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Class:</strong>
+                {{ $result->student_class }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Assessment Type:</strong>
+                {{ $result->assessment_type }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Score:</strong>
+                {{ $result->marks }}
+            </div>
+        </div>
+    </div>
+@endsection
