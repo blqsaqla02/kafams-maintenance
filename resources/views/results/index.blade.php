fbd4e176b3c5cf85e39cd00fe6c09994c7fe7af6
blqsaqla02 <133661551+blqsaqla02@users.noreply.github.com>
2024-12-22 02:07:08 +0800

    searchh

--- resources/views/results/index.blade.php
+++ resources/views/results/index.blade.php
@@ -12,6 +12,20 @@
         </div>
     </div>
    
+<!-- Search Form -->
+<div class="row mb-3">
+        <div class="col-lg-12">
+            <form action="{{ route('results.index') }}" method="GET">
+                <div class="input-group">
+                    <input type="text" name="search" class="form-control" placeholder="Search by student name or class" value="{{ request()->query('search') }}">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-primary">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
     @if ($message = Session::get('success'))
         <div class="alert alert-success">
             <p>{{ $message }}</p>
